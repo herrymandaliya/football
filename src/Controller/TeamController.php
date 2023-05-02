af9192dbd6f8ccf2363292692d8ee8f21a40f7f9
AnkurOO9 <ankur.akvaliya1793@gmail.com>
2023-05-02 16:25:35 +0530

    design improved

---
+++ src/Controller/TeamController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Team;
+use App\Form\TeamType;
+use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
+#[Route('/team')]
+class TeamController extends AbstractController
+{
+    #[Route('/', name: 'app_team_index', methods: ['GET'])]
+    public function index(TeamRepository $teamRepository): Response
+    {
+        return $this->render('team/index.html.twig', [
+            'teams' => $teamRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_team_new', methods: ['GET', 'POST'])]
+    public function new(Request $request ,PersistenceManagerRegistry $doctrine): Response
+    {
+        $team = new Team();
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // print "<pre>";
+            // print_r($this);
+            // print "</pre>";die();
+
+            $entityManager = $doctrine->getManager();
+                
+            $image = $request->files->get('team')['logo'];
+
+            if($image){
+                $dateiname = md5(uniqid()) . '.' . $image->guessClientExtension();
+            }
+
+            $image->move(
+                $this->getParameter('teams'),
+                $dateiname
+            );
+           
+            $team->setLogo($dateiname);
+
+            $entityManager->persist($team);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_team_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('team/new.html.twig', [
+            'team' => $team,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_team_show', methods: ['GET'])]
+    public function show(Team $team): Response
+    {
+        return $this->render('team/show.html.twig', [
+            'team' => $team,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_team_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Team $team, TeamRepository $teamRepository): Response
+    {
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $teamRepository->save($team, true);
+
+            return $this->redirectToRoute('app_team_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('team/edit.html.twig', [
+            'team' => $team,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_team_delete', methods: ['POST'])]
+    public function delete(Request $request, Team $team, TeamRepository $teamRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$team->getId(), $request->request->get('_token'))) {
+            $teamRepository->remove($team, true);
+        }
+
+        return $this->redirectToRoute('app_team_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
