5083b8246218cc83060643f8e1db572f750510f0
unknown <hardik.fullstack@gmail.com>
2023-05-03 12:44:38 +0530

    get team players in dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use App\Repository\PlayerRepository;
 use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +18,18 @@ class DashboardController extends AbstractController
     {
         return $this->render('dashboard/index.html.twig', [
             'teams' => $teamRepository->findAll(),
+        ]);
+    }
+
+    #[Route('team/{id}', name: 'team_details', methods: ['GET'])]
+    public function show(Team $team ,int $id ,PlayerRepository $playerRepository): Response
+    {
+        
+        $teamPlayers = $playerRepository->findPlayersByTeamId($id);
+       
+        return $this->render('dashboard/details.html.twig', [
+            'team' => $team,
+            'teamPlayers'=>$teamPlayers
         ]);
     }
 }
